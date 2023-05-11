db22c9a066965e98deae2c60211670bb6ed7e04d
sik2 <devpang20@gmail.com>
2023-05-11 18:35:40 +0900

    data-*요소 완성

---
+++ script.js
@@ -0,0 +1,8 @@
+class Calculator {
+
+}
+
+const elePreviousPreview = document.querySelector("[data-previous-preview]")
+const eleCurrentPreview = document.querySelector("[data-current-preview]")
+
+const calculator = new Calculator();
